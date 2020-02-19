0c0f1e3e5466d7023f1697eacc8b55c8d6f76092
nathan olenik <nolenik23@outlook.com>
2020-02-19 14:59:47 -0500

    her

--- config/connection.js
+++ config/connection.js
@@ -2,39 +2,25 @@
 const mysql = require("mysql");
 
 
-let connection = {
-    state: false,
-    connect(){
-       this.state = mysql.createConnection({
+let connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else
+    {
+       connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
         password: "Moosemoney7!",
         database: "burgers_db"
       });
-      this.state.connect(function (err) {
-        if (err) {
-          console.error("error connecting: " + err.stack);
-          return;
-        }
-      
-        console.log("connected as id " + connection.threadId);
-        
-      });
-    },
-
-    init(){
-        if (! this.state){
-            this.connect();
-        }
-        return this;
-    }
-}
+    };
 
 
 
 
-
+connection.connect();
 
  module.exports =  connection; 
 
